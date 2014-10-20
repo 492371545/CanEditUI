35eade7a2ce29a7b6166b5ca58cc3ad20de61ea6
492371545 <xmyxumengying@hotmail.com>
2014-10-20 18:01:15 +0800

    Initial Commit

---
+++ CanEditUI-Click/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CanEditUI-Click
+//
+//  Created by Mengying Xu on 14-10-20.
+//  Copyright (c) 2014年 Crystal Xu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CanEditUI-Click/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  CanEditUI-Click
+//
+//  Created by Mengying Xu on 14-10-20.
+//  Copyright (c) 2014年 Crystal Xu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
